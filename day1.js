e4b0806435f9eecd8a4f89c816711772493aabf8
Joshua Seah <joshuacseah@gmail.com>
2016-02-24 19:05:22 -0800

    day1 files

---
+++ day1.js
@@ -0,0 +1,25 @@
+// Geekwise JSI - Day 1 Notes
+// Joshua Seah
+// 2/22/2016
+"use strict";
+
+var favFood = "Cheeseburger";
+var favDrink = "Water";
+var favFoodAndDrink = favFood + ", " + favDrink;
+
+console.log(favFoodAndDrink);
+
+var num1 = 27;
+var num2 = 15;
+var num3 = 42;
+
+var sum = num1 + num2 + num3;
+var average = sum / 3;
+
+console.log(sum);
+console.log(average);
+
+var numArray = [27, 15, 42];
+sum = numArray[0] + numArray[1] + numArray[2];
+
+console.log(sum);
